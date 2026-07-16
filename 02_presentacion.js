b0c77d9e7a6d3673e8fd593d31cd5f3a33a36c6e
Alejandro Valencia Mendoza <alejandro.valencia.mdza@gmail.com>
2026-07-16 13:31:31 -0600

    Creacion de los programs 2 y 3 de js

---
+++ 02_presentacion.js
@@ -0,0 +1,11 @@
+/* 
+Reto #2
+
+Nombre del archivo: 02_presentacion.js
+
+Instrucciones:
+Realiza un programa que muestre en la consola y en una ventana de alerta tu nombre, tu edad y tu ciudad. Cada dato debe mostrarse por separado.
+*/
+
+console.log("ALEJANDRO");
+alert("ALEJANDRO");
---
+++ 03_variables.js
@@ -0,0 +1,22 @@
+/* 
+Reto #3
+
+Nombre del archivo: 03_variables.js
+
+Instrucciones:
+Declara tres variables para guardar tu nombre, tu edad y tu comida favorita.
+
+Después, muestra el contenido de cada variable en la consola y en una ventana de alerta.
+
+No escribas los datos directamente al mostrarlos; utiliza las variables.
+*/
+
+let nombre = "alejandro";
+let edad = 26;
+let comidaf = "pizza";
+
+console.log(`tu nombre es: ${nombre}`);
+console.log(`tu edad es: ${edad}`);
+console.log(`tu comida favorita es: ${comidaf}`);
+
+alert("Tu nombre es: "+nombre+" \nTu edad es: "+edad+" \nTu comida favorita es: "+comidaf);
