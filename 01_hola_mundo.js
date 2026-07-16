58d653db3255edf0be0273b59ff5adee15696ac2
Alejandro Valencia Mendoza <alejandro.valencia.mdza@gmail.com>
2026-07-16 13:10:02 -0600

    creacion del primer programa js y creacion de la base HTML5

---
+++ 01_hola_mundo.js
@@ -0,0 +1,11 @@
+/* 
+Reto #1
+
+Nombre del archivo: 01_hola_mundo.js
+
+Instrucciones:
+Realiza un programa que muestre "Hola Mundo" en la consola y en una ventana de alerta.
+*/
+
+console.log("Hola Mundo");
+alert("Hola Mundo");
